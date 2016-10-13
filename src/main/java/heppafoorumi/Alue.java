6dbca067464a059f5f51bb69ef4b6257714d2284
Matti Vikberg <Matti Vikberg>
2016-10-13 13:01:41 +0300

    Aloitettu heppafoorumin javakoodi tikaperungon pohjalta
    	new file:   src/main/java/heppafoorumi/Alue.java
    	new file:   src/main/java/heppafoorumi/AlueDao.java
    	renamed:    src/main/java/tikape/runko/database/OpiskelijaDao.java -> src/main/java/tikape/runko/database/AlueDao.java

---
+++ src/main/java/heppafoorumi/Alue.java
@@ -0,0 +1,30 @@
+package heppafoorumi;
+
+public class Alue {
+
+    private Integer id;
+    private String nimi;
+
+    public Alue(Integer id, String nimi) {
+        this.id = id;
+        this.nimi = nimi;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+}
+
---
+++ src/main/java/heppafoorumi/AlueDao.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heppafoorumi;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import tikape.runko.database.Database;
+import tikape.runko.database.Dao;
+public class AlueDao implements Dao<Alue, Integer> {
+
+    private Database database;
+
+    public AlueDao(Database database) {
+        this.database = database;
+    }
+
+    @Override
+    public Alue findOne(Integer key) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Alue WHERE id = ?");
+        stmt.setObject(1, key);
+
+        ResultSet rs = stmt.executeQuery();
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+
+        Integer id = rs.getInt("id");
+        String nimi = rs.getString("nimi");
+
+        Alue o = new Alue(id, nimi);
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return o;
+    }
+
+    @Override
+    public List<Alue> findAll() throws SQLException {
+
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Alue");
+
+        ResultSet rs = stmt.executeQuery();
+        List<Alue> opiskelijat = new ArrayList<>();
+        while (rs.next()) {
+            Integer id = rs.getInt("id");
+            String nimi = rs.getString("nimi");
+
+            opiskelijat.add(new Alue(id, nimi));
+        }
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return opiskelijat;
+    }
+
+    @Override
+    public void delete(Integer key) throws SQLException {
+        // ei toteutettu
+    }
+
+}
