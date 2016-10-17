e67e462b04e538961572ca8a340f0aca64fecb02
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-10-18 01:28:37 +0300

    Pääohjelmaluokka, jonka voi käynnistää.
    Optiona vanhojen portissa 4567 pyörivien palvelinprosessien lopetus
    (kehityksen nopeuttamiseksi, toistaiseksi vain Linuxilla).
    Linuxillakin sen saa halutessaan pois päältä asettamalla
    `boolean lopetetaankoVanhatPalvelinprosessit = false;`.
    
    Kääntyy mutta metodin `public List<Alue> findAll()` rivi
    `Connection connection = database.getConnection();` tuottaa seuraavan
    virheilmoituksen:
    
    Exception in thread "main" org.h2.jdbc.JdbcSQLException: Wrong user name
    or password [28000-190]
    
    Ongelma liittyy siis tietokannan käyttäjätunnuksiin ja salasanoihin.
    
    	modified:   pom.xml
    	new file:   src/main/java/heppafoorumi/Heppafoorumi.java

---
+++ src/main/java/heppafoorumi/Heppafoorumi.java
@@ -0,0 +1,102 @@
+package heppafoorumi;
+
+import heppafoorumi.dao.AiheDao;
+import heppafoorumi.dao.AlueDao;
+import heppafoorumi.dao.ViestiDao;
+import heppafoorumi.domain.Alue;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.HashMap;
+import java.util.List;
+import spark.ModelAndView;
+import static spark.Spark.get;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+import heppafoorumi.database.Database;
+import org.h2.tools.RunScript;
+
+public class Heppafoorumi {
+
+    private static boolean onkoLinux() {
+        // http://stackoverflow.com/questions/3282498/how-can-i-detect-a-unix-like-os-in-java/3282597#3282597
+        // Oletetaan että jos ei ole Windows, niin on Linux.
+        return (!(System.getProperty("os.name").startsWith("Windows")));
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        // alla oleva koodi on kehityksen nopeuttamiseksi,
+        // kun ei tarvitse huolehtia vanhoista palvelinprosesseista.
+        boolean lopetetaankoVanhatPalvelinprosessit = true;
+
+        if (lopetetaankoVanhatPalvelinprosessit && onkoLinux()) {
+            // lopetetaan vanha palvelin, toimii Linuxissa.
+            // jos käyttöjärjestelmä on Windows, tätä lohkoa ei suoriteta.
+
+            // tällä tavoin tehtynä vanhojen prosessien niittaus toimii
+            // (run toimii, clean & build myös).
+            try (PrintWriter writer = new PrintWriter("/tmp/niittaa_spark", "UTF-8")) {
+
+                // kirjoitetaan tiedostoon /tmp/niittaa_spark bash-skripti joka
+                // lopettaa porttia 4567 kuuntelevat prosessit.
+                writer.println("#!/bin/bash");
+                writer.println("sh -c 'for i in $(lsof -ti :4567); do kill -9 $i; done'");
+                writer.close();
+
+                // määritellään käynnistettävä ohjelma. 
+                String[] cmd = new String[]{"/bin/sh", "/tmp/niittaa_spark"};
+
+                try {
+                    // käynnistetään äsken luotu bash-skripti.
+                    Process prosessi = Runtime.getRuntime().exec(cmd);
+                    // odotetaan että skriptin suoritus päättyy.
+                    prosessi.waitFor();
+                } catch (IOException | InterruptedException e) {
+                    // ei tehdä mitään poikkeuksille.
+                }
+            } finally {
+                // poistetaan äsken luotu bash-skripti, onnistui lopetus tai ei.
+                File tiedosto = new File("/tmp/niittaa_spark");
+                tiedosto.delete();
+            }
+        }
+
+        // no niin, vanhat palvelinprosessit lopetettu, siirrytään asiaan.
+        // Open connection to a database
+        Connection connection = DriverManager.getConnection("jdbc:h2:./database", "sa", "");
+
+        try {
+            // If database has not yet been created, insert content
+            RunScript.execute(connection, new FileReader("database-schema.sql"));
+            RunScript.execute(connection, new FileReader("database-import.sql"));
+        } catch (Throwable t) {
+        }
+        connection.close();
+
+        Database database = new Database("jdbc:h2:./database");
+        AlueDao alueDao = new AlueDao(database);
+        AiheDao aiheDao = new AiheDao(database);
+        ViestiDao viestiDao = new ViestiDao(database);
+
+        // tulostetaan kaikki alueet konsoliin.
+        System.out.println("Kaikki alueet");
+
+        List<Alue> alueet = alueDao.findAll();
+
+        for (Alue alue : alueet) {
+            System.out.println(alue);
+        }
+
+        // lambda-lausekkeet HTTP-pyyntöjen käsittelyä varten.
+        // Heppafoorumin pääsivu.
+        get("/", (request, response) -> {
+            HashMap<String, Object> data = new HashMap();
+            data.put("item", alueet);
+            return new ModelAndView(data, "index");
+        }, new ThymeleafTemplateEngine());
+
+    }
+}
