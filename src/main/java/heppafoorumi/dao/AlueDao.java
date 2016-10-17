7895212c7bfe0b6b0f3c336caa58d8b9aff348f5
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-10-17 21:50:10 +0300

    AlueDao:n muuttujien nimet muutettu luettavammiksi.
    `private Database database;` -> `private final Database database;`.
    
    	modified:   src/main/java/heppafoorumi/dao/AlueDao.java

--- src/main/java/heppafoorumi/dao/AlueDao.java
+++ src/main/java/heppafoorumi/dao/AlueDao.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package heppafoorumi.dao;
 
 import heppafoorumi.domain.Alue;
@@ -14,9 +9,10 @@ import java.util.ArrayList;
 import java.util.List;
 import tikape.runko.database.Database;
 import tikape.runko.database.Dao;
+
 public class AlueDao implements Dao<Alue, Integer> {
 
-    private Database database;
+    private final Database database;
 
     public AlueDao(Database database) {
         this.database = database;
@@ -25,25 +21,25 @@ public class AlueDao implements Dao<Alue, Integer> {
     @Override
     public Alue findOne(Integer key) throws SQLException {
         Connection connection = database.getConnection();
-        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Alue WHERE id = ?");
-        stmt.setObject(1, key);
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM Alue WHERE id = ?");
+        statement.setObject(1, key);
 
-        ResultSet rs = stmt.executeQuery();
-        boolean hasOne = rs.next();
+        ResultSet resultSet = statement.executeQuery();
+        boolean hasOne = resultSet.next();
         if (!hasOne) {
             return null;
         }
 
-        Integer id = rs.getInt("id");
-        String nimi = rs.getString("nimi");
+        Integer id = resultSet.getInt("id");
+        String nimi = resultSet.getString("nimi");
 
-        Alue o = new Alue(id, nimi);
+        Alue alue = new Alue(id, nimi);
 
-        rs.close();
-        stmt.close();
+        resultSet.close();
+        statement.close();
         connection.close();
 
-        return o;
+        return alue;
     }
 
     @Override
@@ -52,25 +48,24 @@ public class AlueDao implements Dao<Alue, Integer> {
         Connection connection = database.getConnection();
         PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Alue");
 
-        ResultSet rs = stmt.executeQuery();
-        List<Alue> opiskelijat = new ArrayList<>();
-        while (rs.next()) {
-            Integer id = rs.getInt("id");
-            String nimi = rs.getString("nimi");
+        ResultSet resultSet = stmt.executeQuery();
+        List<Alue> alueet = new ArrayList<>();
+        while (resultSet.next()) {
+            Integer id = resultSet.getInt("id");
+            String nimi = resultSet.getString("nimi");
 
-            opiskelijat.add(new Alue(id, nimi));
+            alueet.add(new Alue(id, nimi));
         }
 
-        rs.close();
+        resultSet.close();
         stmt.close();
         connection.close();
 
-        return opiskelijat;
+        return alueet;
     }
 
     @Override
     public void delete(Integer key) throws SQLException {
         // ei toteutettu
     }
-
 }
