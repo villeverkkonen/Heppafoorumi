10a743637f7eab9c960391c2794b83429b886d78
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-10-20 00:28:39 +0300

    Lisätty otsikkosarake: `database-import.sql` & `database-schema.sql`.
    	modified:   database-import.sql
    	modified:   database-schema.sql

--- database-schema.sql
+++ database-schema.sql
@@ -2,6 +2,7 @@ CREATE TABLE Alue
 (
     id int PRIMARY KEY,
     aikaleima date,
+    otsikko varchar(200),
     teksti varchar(200)
 );
 
@@ -11,16 +12,18 @@ CREATE TABLE Aihe
     aikaleima date,
     alue int,
     nimimerkki varchar(20),
-    teksti varchar(40),
+    otsikko varchar(200),
+    teksti varchar(200),
     FOREIGN KEY(alue) REFERENCES Alue(id),
 );
 
-CREATE TABLE Viesti 
+CREATE TABLE Viesti
 (
-    id int PRIMARY KEY, 
-    aikaleima date, 
-    aihe int, 
-    nimimerkki varchar(20), 
-    teksti varchar(200), 
+    id int PRIMARY KEY,
+    aikaleima date,
+    aihe int,
+    nimimerkki varchar(20),
+    otsikko varchar(200),
+    teksti varchar(200),
     FOREIGN KEY(aihe) REFERENCES Aihe(id)
 );
