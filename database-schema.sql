92074dc2f8555330718ce5eef524e53695edde52
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-10-20 02:25:43 +0300

    Korjattu `database-import.sql` & `database-schema.sql`.
    	modified:   database-import.sql
    	modified:   database-schema.sql

--- database-schema.sql
+++ database-schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Alue
 (
     id int PRIMARY KEY,
-    aikaleima date,
+    aikaleima datetime(3),
     otsikko varchar(200),
     teksti varchar(200)
 );
@@ -9,21 +9,20 @@ CREATE TABLE Alue
 CREATE TABLE Aihe
 (
     id int PRIMARY KEY,
-    aikaleima date,
+    aikaleima datetime(3),
     alue int,
     nimimerkki varchar(20),
     otsikko varchar(200),
     teksti varchar(200),
-    FOREIGN KEY(alue) REFERENCES Alue(id),
+    FOREIGN KEY(alue) REFERENCES Alue(id)
 );
 
 CREATE TABLE Viesti
 (
     id int PRIMARY KEY,
-    aikaleima date,
+    aikaleima datetime(3),
     aihe int,
     nimimerkki varchar(20),
-    otsikko varchar(200),
     teksti varchar(200),
     FOREIGN KEY(aihe) REFERENCES Aihe(id)
 );
