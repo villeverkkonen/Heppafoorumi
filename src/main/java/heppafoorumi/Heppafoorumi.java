179117e39cd501d953b702f423937771f414a90c
Ville Verkkonen <villeverkkonen@gmail.com>
2016-10-19 13:48:19 +0300

    Etusivun alueet-linkeistä aukeaa aihe.html

--- src/main/java/heppafoorumi/Heppafoorumi.java
+++ src/main/java/heppafoorumi/Heppafoorumi.java
@@ -19,7 +19,7 @@ public class Heppafoorumi {
 
     private static boolean onkoLinux() {
         // http://stackoverflow.com/questions/3282498/how-can-i-detect-a-unix-like-os-in-java/3282597#3282597
-        // Oletetaan että jos ei ole Windows, niin on Linux.
+        // Oletetaan ettÃ¤ jos ei ole Windows, niin on Linux.
         return (!(System.getProperty("os.name").startsWith("Windows")));
     }
 
@@ -31,10 +31,10 @@ public class Heppafoorumi {
 
         if (lopetetaankoVanhatPalvelinprosessit && onkoLinux()) {
             // lopetetaan vanha palvelin, toimii Linuxissa.
-            // jos käyttöjärjestelmä on Windows, tätä lohkoa ei suoriteta.
+            // jos kÃ¤yttÃ¶jÃ¤rjestelmÃ¤ on Windows, tÃ¤tÃ¤ lohkoa ei suoriteta.
 
-            // tällä tavoin tehtynä vanhojen prosessien niittaus toimii
-            // (run toimii, clean & build myös).
+            // tÃ¤llÃ¤ tavoin tehtynÃ¤ vanhojen prosessien niittaus toimii
+            // (run toimii, clean & build myÃ¶s).
             try (PrintWriter writer = new PrintWriter("/tmp/niittaa_spark", "UTF-8")) {
 
                 // kirjoitetaan tiedostoon /tmp/niittaa_spark bash-skripti joka
@@ -43,25 +43,25 @@ public class Heppafoorumi {
                 writer.println("sh -c 'for i in $(lsof -ti :4567); do kill -9 $i; done'");
                 writer.close();
 
-                // määritellään käynnistettävä ohjelma. 
+                // mÃ¤Ã¤ritellÃ¤Ã¤n kÃ¤ynnistettÃ¤vÃ¤ ohjelma. 
                 String[] cmd = new String[]{"/bin/sh", "/tmp/niittaa_spark"};
 
                 try {
-                    // käynnistetään äsken luotu bash-skripti.
+                    // kÃ¤ynnistetÃ¤Ã¤n Ã¤sken luotu bash-skripti.
                     Process prosessi = Runtime.getRuntime().exec(cmd);
-                    // odotetaan että skriptin suoritus päättyy.
+                    // odotetaan ettÃ¤ skriptin suoritus pÃ¤Ã¤ttyy.
                     prosessi.waitFor();
                 } catch (IOException | InterruptedException e) {
-                    // ei tehdä mitään poikkeuksille.
+                    // ei tehdÃ¤ mitÃ¤Ã¤n poikkeuksille.
                 }
             } finally {
-                // poistetaan äsken luotu bash-skripti, onnistui lopetus tai ei.
+                // poistetaan Ã¤sken luotu bash-skripti, onnistui lopetus tai ei.
                 File tiedosto = new File("/tmp/niittaa_spark");
                 tiedosto.delete();
             }
         }
 
-        // no niin, vanhat palvelinprosessit lopetettu, siirrytään asiaan.
+        // no niin, vanhat palvelinprosessit lopetettu, siirrytÃ¤Ã¤n asiaan.
         Database database = new Database("jdbc:h2:./database");
 
         AlueDao alueDao = new AlueDao(database);
@@ -77,20 +77,54 @@ public class Heppafoorumi {
             System.out.println(alue);
         }
 
-        Alue testialue1 = new Alue(0, 0, "testialue");
-        alueet.add(testialue1);
+        Alue alue1 = new Alue(0, 2016, "Ponit");
+        alueet.add(alue1);
+        
+        Alue alue2 = new Alue(1, 2016, "Offtopic");
+        alueet.add(alue2);
+        
+//        Aihe aihe1 = new Aihe(1, 2016, alue1, "Ponit on perseestÃ¤!!!", "En tykkÃ¤Ã¤ poneista.");
 
-        Alue testialue2 = new Alue(1, 1, "toinen testialue");
-        alueet.add(testialue2);
-
-        // lambda-lausekkeet HTTP-pyyntöjen käsittelyä varten.
-        // Heppafoorumin pääsivu.
+        // lambda-lausekkeet HTTP-pyyntÃ¶jen kÃ¤sittelyÃ¤ varten.
+        // Heppafoorumin pÃ¤Ã¤sivu.
         get("/", (request, response) -> {
             HashMap<String, Object> data = new HashMap();
             data.put("alueet", alueet);
             return new ModelAndView(data, "alueet");
         }, new ThymeleafTemplateEngine());
         
+        get("/:alue", (request, response) -> {
+            HashMap<String, Object> data = new HashMap();
+            data.put("alue", data.get(request.params(":alue")));
+            return new ModelAndView(data, "aiheet");
+        }, new ThymeleafTemplateEngine());
+  
+        
+//        tulostetaan kaikki aiheet konsoliin.
+//        System.out.println("Kaikki alueet");
+//
+//        List<Aihe> aiheet = new List<>();
+//
+//        for (Alue alue : alueet) {
+//            System.out.println(alue);
+//        }
+//
+//      Aihe aihe1 = new Aihe(0, 2016, "My Litlle Pony");
+//        alueet.add(aihe1);
+//
+//        Aihe aihe2 = new Aihe(1, 2016, "Islanninponit");
+//        alueet.add(aihe2);
+//  
+//        Aihe aihe3 = new Aihe(2, 2016, "Ponit on perseestÃ¤!!!");
+//        alueet.add(aihe3);
+//
+// lambda-lausekkeet HTTP-pyyntÃ¶jen kÃ¤sittelyÃ¤ varten.
+// Heppafoorumin pÃ¤Ã¤sivu.
+//        get("/", (request, response) -> {
+//            HashMap<String, Object> data = new HashMap();
+//            data.put("aiheett", aiheet);
+//            return new ModelAndView(data, "aiheet");
+//        }, new ThymeleafTemplateEngine());
         
     }
 }
