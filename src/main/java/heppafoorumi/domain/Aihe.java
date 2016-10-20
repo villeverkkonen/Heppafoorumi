c2f50840cec591d7812bb0cea7a84946f8bf7500
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-10-20 19:14:58 +0300

    Revert "Alue-, Aihe- ja Viesti-olioiden id:t luodaan tietokannan perusteella."
    This reverts commit 01ddd475db03fbec93d063664a237f043eb2b3c2.
    
    Conflicts:
    	src/main/java/heppafoorumi/Heppafoorumi.java
    	src/main/java/heppafoorumi/dao/AlueDao.java

--- src/main/java/heppafoorumi/domain/Aihe.java
+++ src/main/java/heppafoorumi/domain/Aihe.java
@@ -1,9 +1,5 @@
 package heppafoorumi.domain;
 
-import heppafoorumi.database.Database;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Timestamp;
 
 public class Aihe extends Kategoria {
@@ -16,49 +12,33 @@ public class Aihe extends Kategoria {
     // otsikko varchar(200),
     // teksti varchar(200),
     // FOREIGN KEY(alue) REFERENCES Alue(id);
-    private final int alue;
+    private final Alue alue; // huom. Alue-olio, vrt. wepa:n 28. HelloOneToMany:n Agent.java
     private final String otsikko;
-
-    private final Database database;
 
     // ainoastaan 20 ensimmäistä merkkiä otetaan huomioon.
     private final String nimimerkki;
 
-    private static int getNewAiheId(Database database) throws SQLException {
-        Connection connection = database.getConnection();
-        int id;
-        ResultSet resultSet = connection.createStatement().executeQuery(
-                "SELECT id FROM Aihe ORDER BY id DESC LIMIT 1");
-        if (resultSet.next()) {
-            id = resultSet.getInt("id") + 1;
-        } else {
-            id = 1;
-        }
-        resultSet.close();
-        return id;
-    }
-
-    public Aihe(Database database, Integer id, Timestamp aikaleima, int alueId, String nimimerkki, String otsikko, String teksti) {
+    public Aihe(Integer id, Timestamp aikaleima, Alue alue, String nimimerkki, String otsikko, String teksti) {
         super(id, aikaleima, teksti);
-        this.database = database;
-        this.alue = alueId;
+        this.alue = alue;
 
         // tallennetaan enintään 20 ensimmäistä merkkiä syötetystä nimimerkistä.
         this.nimimerkki = nimimerkki.substring(Math.min(nimimerkki.length() - 1, NIMIMERKIN_PITUUS));
         this.otsikko = otsikko.substring(Math.min(otsikko.length() - 1, OTSIKON_PITUUS));
     }
 
-    public Aihe(Database database, Integer id, int alueId, String nimimerkki, String otsikko, String teksti) {
-        this(database, id, new java.sql.Timestamp(new java.util.Date().getTime()), alueId, nimimerkki, otsikko, teksti);
+    public Aihe(Integer id, Alue alue, String nimimerkki, String otsikko, String teksti) {
+        this(id, new java.sql.Timestamp(new java.util.Date().getTime()), alue, nimimerkki, otsikko, teksti);
+    }
+    
+    public Aihe(Alue alue, String nimimerkki, String otsikko, String teksti) {
+        this(-1, alue, nimimerkki, otsikko, teksti);
     }
 
-    public Aihe(Database database, int alueId, String nimimerkki, String otsikko, String teksti) throws SQLException {
-        this(database, Aihe.getNewAiheId(database), alueId, nimimerkki, otsikko, teksti);
+    public Alue getAlue() {
+        return this.alue;
     }
 
-//    public Alue getAlue() {
-//        // return this.alue;
-//    }
     public String getNimimerkki() {
         return this.nimimerkki;
     }
