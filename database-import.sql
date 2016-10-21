f4a3afbd0dacb69693f4125ddebb0f0666cc5fb6
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-10-21 19:25:34 +0300

    Lisätty ja muokattu viestejä ja aiheita debuggauksen helpottamiseksi.
    	modified:   database-import.sql

--- database-import.sql
+++ database-import.sql
@@ -2,11 +2,13 @@ INSERT INTO Alue (aikaleima, otsikko, teksti) VALUES ('2016-10-20 01:07:00', 'Po
 INSERT INTO Alue (aikaleima, otsikko, teksti) VALUES ('2016-10-20 01:07:01', 'Hevoiset', 'Hevoslajit');
 INSERT INTO Alue (aikaleima, otsikko, teksti) VALUES ('2016-10-20 01:07:01', 'Vikellys', 'Akrobatiaa liikkuvan hevosen selässä');
 INSERT INTO Alue (aikaleima, otsikko, teksti) VALUES ('2016-10-20 01:07:02', 'Offtopic', 'Kaikkea maan ja taivaan väliltä');
-INSERT INTO Aihe (aikaleima, alue, nimimerkki, otsikko, teksti) VALUES ('2016-10-20 01:07:03', 1, 'nuorzi', 'Ponit ovat parhaat', '(puuttuu)');
-INSERT INTO Aihe (aikaleima, alue, nimimerkki, otsikko, teksti) VALUES ('2016-10-20 01:07:04', 2, 'zille', 'Islanninhevoset', '(puuttuu)');
-INSERT INTO Aihe (aikaleima, alue, nimimerkki, otsikko, teksti) VALUES ('2016-10-20 01:07:05', 3, 'zille', 'Pokemon-ponit', '(puuttuu)');
-INSERT INTO Aihe (aikaleima, alue, nimimerkki, otsikko, teksti) VALUES ('2016-10-20 01:07:06', 4, 'nuorzi', 'Ponit on ihania!!!', 'Eiks ookin???');
+INSERT INTO Aihe (aikaleima, alue, nimimerkki, otsikko, teksti) VALUES ('2016-10-20 01:07:03', 1, 'nuorzi', 'Ponit ovat parhaat', 'Ei varmaan oo kukaan eri mieltä?');
+INSERT INTO Aihe (aikaleima, alue, nimimerkki, otsikko, teksti) VALUES ('2016-10-20 01:07:04', 2, 'zille', 'Islanninhevoset', 'Islanninhevoset #2');
+INSERT INTO Aihe (aikaleima, alue, nimimerkki, otsikko, teksti) VALUES ('2016-10-20 01:07:05', 1, 'zille', 'Pokemon-ponit', 'Pokemon-ponit #2');
+INSERT INTO Aihe (aikaleima, alue, nimimerkki, otsikko, teksti) VALUES ('2016-10-20 01:07:06', 1, 'nuorzi', 'Ponit on ihania!!!', 'Eiks ookin???');
 INSERT INTO Aihe (aikaleima, alue, nimimerkki, otsikko, teksti) VALUES ('2016-10-20 01:07:07', 1, 'nuorzi', 'Ponit on söpöjä!!!', 'Onks kaikki samaa mieltä???');
+INSERT INTO Aihe (aikaleima, alue, nimimerkki, otsikko, teksti) VALUES ('2016-10-21 19:19:23', 4, 'nuorzi', 'Yks juttu mikä tuli mieleen',
+    'Eilen Helsingin sisäisessä bussissa yks lapsi selitti vaarilleen mikä on Pokemon. Siinä on kai jotain poke-palloja, joita heitetään.');
 INSERT INTO Viesti (aikaleima, aihe, nimimerkki, teksti) VALUES ('2016-10-20 01:07:08', 3, 'zille', 'Charmander on paras Pokemon-poni');
 INSERT INTO Viesti (aikaleima, aihe, nimimerkki, teksti) VALUES ('2016-10-20 01:07:09', 2, 'zille', 'En tykkää islanninhevosista...');
 INSERT INTO Viesti (aikaleima, aihe, nimimerkki, teksti) VALUES ('2016-10-20 01:07:10', 2, 'nuorzi', 'Siis onko muka joku joka ei tykkää islanninhevosista???');
