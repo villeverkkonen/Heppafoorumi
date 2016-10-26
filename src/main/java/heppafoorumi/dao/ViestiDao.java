49e38657bc2979bec9060b6c2c6c0f7a8c467541
Antti Nuortimo <antti.nuortimo@gmail.com>
2016-10-27 00:57:12 +0300

    Funktion `public Viesti findOne(Integer key)` siivousta.
    Kyseinen funktio ei ole käytössä tällä hetkellä. Ei toiminnallisia
    muutoksia.
    
    	modified:   src/main/java/heppafoorumi/dao/ViestiDao.java

--- src/main/java/heppafoorumi/dao/ViestiDao.java
+++ src/main/java/heppafoorumi/dao/ViestiDao.java
@@ -24,57 +24,29 @@ public class ViestiDao implements Dao<Viesti, Integer> {
     public Viesti findOne(Integer key) throws SQLException {
         Connection connection = database.getConnection();
         ResultSet resultSet = connection.createStatement().executeQuery("SELECT "
-                + "alue.id AS alue_id, "
-                + "alue.aikaleima AS alue_aikaleima, "
-                + "alue.otsikko AS alue_otsikko, "
-                + "alue.teksti AS alue_teksti, "
-                + "aihe.id AS aihe_id, "
-                + "aihe.aikaleima AS aihe_aikaleima, "
-                + "aihe.alue AS aihe_alue, "
-                + "aihe.nimimerkki AS aihe_nimimerkki, "
-                + "aihe.otsikko AS aihe_otsikko, "
-                + "aihe.teksti AS aihe_teksti, "
                 + "viesti.id AS viesti_id, "
                 + "viesti.aikaleima AS viesti_aikaleima, "
                 + "viesti.aihe AS viesti_aihe, "
                 + "viesti.nimimerkki AS viesti_nimimerkki, "
                 + "viesti.teksti AS viesti_teksti "
-                + "FROM Alue alue, Aihe aihe, Viesti viesti "
-                + "WHERE aihe.alue = alue.id AND "
-                + "viesti.aihe = aihe.id AND "
-                + "viesti.id = '" + key + "'");
+                + "FROM Viesti viesti "
+                + "WHERE viesti.id = " + key);
 
         boolean hasOne = resultSet.next();
         if (!hasOne) {
             return null;
         }
 
-        Integer alueId = resultSet.getInt("alue_id");
-        Timestamp alueAikaleima = resultSet.getTimestamp("alue_aikaleima");
-        String alueOtsikko = resultSet.getString("alue_otsikko");
-        String alueTeksti = resultSet.getString("alue_teksti");
-
-        Integer aiheId = resultSet.getInt("aihe_id");
-        Timestamp aiheAikaleima = resultSet.getTimestamp("aihe_aikaleima");
-        // Integer aiheAlue = resultSet.getInt("aihe_alue");
-        String aiheNimimerkki = resultSet.getString("aihe_nimimerkki");
-        String aiheOtsikko = resultSet.getString("aihe_otsikko");
-        String aiheTeksti = resultSet.getString("aihe_teksti");
-
         Integer viestiId = resultSet.getInt("viesti_id");
         Timestamp viestiAikaleima = resultSet.getTimestamp("viesti_aikaleima");
-        // Integer viestiAihe = resultSet.getInt("viesti_aihe");
+        Integer viestiAihe = resultSet.getInt("viesti_aihe");
         String viestiNimimerkki = resultSet.getString("viesti_nimimerkki");
         String viestiTeksti = resultSet.getString("viesti_teksti");
 
         resultSet.close();
         connection.close();
 
-        Alue alue = new Alue(this.database, alueId, alueAikaleima, alueOtsikko, alueTeksti);
-
-        Aihe aihe = new Aihe(this.database, aiheId, aiheAikaleima, alueId, aiheNimimerkki, aiheOtsikko, aiheTeksti);
-
-        Viesti viesti = new Viesti(this.database, viestiId, viestiAikaleima, aiheId, viestiNimimerkki, viestiTeksti);
+        Viesti viesti = new Viesti(this.database, viestiId, viestiAikaleima, viestiAihe, viestiNimimerkki, viestiTeksti);
 
         resultSet.close();
         connection.close();
