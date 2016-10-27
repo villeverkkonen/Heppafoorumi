892b6cc27e383f51f34e956db315d206e4d4e2fb
Ville Verkkonen <villeverkkonen@gmail.com>
2016-10-27 13:32:25 +0300

    Aiheraportti.javan lisäys

---
+++ src/main/java/heppafoorumi/domain/Aiheraportti.java
@@ -0,0 +1,70 @@
+package heppafoorumi.domain;
+
+import java.sql.Timestamp;
+
+public class Aiheraportti {
+    private final Aihe aihe;
+    private final Viesti viesti;
+
+    // Raporttiluokka aluenäkymän luontia varten.
+    public Aiheraportti(Aihe aihe, Viesti viesti) {
+        this.aihe = aihe;
+        this.viesti = viesti;
+    }
+
+    public Integer getAiheId() {
+        if (this.aihe == null) {
+            return null;
+        }
+        return this.aihe.getId();
+    }
+
+    public String getAiheNimimerkki() {
+        if (this.aihe == null) {
+            return null;
+        }
+        return this.aihe.getNimimerkki();
+    }
+
+    public String getAiheOtsikko() {
+        if (this.aihe == null) {
+            return null;
+        }
+        return this.aihe.getOtsikko();
+    }
+
+    public String getAiheTeksti() {
+        if (this.aihe == null) {
+            return null;
+        }
+        return this.aihe.getTeksti();
+    }
+
+    public Timestamp getAiheAikaleima() {
+        if (this.aihe == null) {
+            return null;
+        }
+        return this.aihe.getAikaleima();
+    }
+
+    public String getViestiNimimerkki() {
+        if (this.aihe == null) {
+            return "";
+        }
+        return this.viesti.getNimimerkki();
+    }
+
+    public String getViestiTeksti() {
+        if (this.aihe == null) {
+            return null;
+        }
+        return this.viesti.getTeksti();
+    }
+
+    public Timestamp getViestiAikaleima() {
+        if (this.aihe == null) {
+            return null;
+        }
+        return this.aihe.getAikaleima();
+    }
+}
